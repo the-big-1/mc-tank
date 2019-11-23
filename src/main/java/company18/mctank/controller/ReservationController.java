1f7f9125820bfa2462a7bcb66ffc2b760654df86
chsc949a <christoph.schroeter1@mailbox.tu-dresden.de>
2019-11-23 19:19:54 +0100

    split mcwash and mcsit reservations

--- src/main/java/company18/mctank/controller/ReservationController.java
+++ src/main/java/company18/mctank/controller/ReservationController.java
@@ -9,6 +9,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import company18.mctank.domain.Reservation;
+import company18.mctank.domain.McSitReservation;
+import company18.mctank.domain.McWashReservation;
 import company18.mctank.forms.ReservationForm;
 import company18.mctank.service.ReservationService;
 
@@ -23,8 +25,10 @@ public class ReservationController {
 	
 	@GetMapping(value="/reservation")
 	public String reservations(Model model) {
-		Iterable<Reservation> reservs = ReservationService.sortByDate(reservationService.findAll());
-		model.addAttribute("reservations", reservs);
+		Iterable<Reservation> reservs = ReservationService.sortByDate(reservationService.findByClass(McSitReservation.class));
+		model.addAttribute("mcsitreservations", reservs);
+		reservs = ReservationService.sortByDate(reservationService.findByClass(McWashReservation.class));
+		model.addAttribute("mcwashreservations", reservs);
 		model.addAttribute("form", new ReservationForm());
 		return "reservation";
 	}
