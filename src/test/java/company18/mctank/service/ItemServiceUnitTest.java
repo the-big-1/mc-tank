f36012a9c2763aa4bab7b5a8af4a97352d504690
davlei <david.leistner@tu-dresden.de>
2019-11-27 19:15:28 +0100

    added items unittest

---
+++ src/test/java/company18/mctank/service/ItemServiceUnitTest.java
@@ -0,0 +1,36 @@
+package company18.mctank.service;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import company18.mctank.forms.NewItemForm;
+import company18.mctank.repository.Items;
+
+class ItemServiceUnitTest{
+	
+	@Test
+	void createNewProductTest() {
+		
+		Items items = Mockito.mock(Items.class);
+		Mockito.when(items.save(Mockito.any())).then(i -> i.getArgument(0));
+		
+		
+		ItemsService service = new ItemsService(items);
+		
+		var form = new NewItemForm("prod1", "1.30", new ArrayList<String>());
+		
+		assertThat(service.createNewProduct(form)).isNotNull();  				// testing if an new Product is created
+		
+		//test various
+		
+		// test if adding Categories works properly
+		
+		
+		
+	}
+}
