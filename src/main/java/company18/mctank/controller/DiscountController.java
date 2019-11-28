5881d23960debcb4cdcf8056d90b20a2a445017b
Vivien <Vivien.Landgrebe@mailbox.tu-dresden.de>
2019-11-28 08:32:14 +0100

    added pay function

--- src/main/java/company18/mctank/controller/DiscountController.java
+++ src/main/java/company18/mctank/controller/DiscountController.java
@@ -1,11 +1,13 @@
 package company18.mctank.controller;
 
 import company18.mctank.domain.DiscountCart;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.salespointframework.order.Cart;
 
 @Controller
 public class DiscountController {
@@ -23,7 +25,21 @@ public class DiscountController {
 
 	@PostMapping(value="/discount")
 	public String addDiscount(@RequestParam("discountCode") String discountCode){
-		cart.addDiscount(discountCode);
+		cart.addDiscount(discountCode); 
 		return "redirect:/discount";
-	}
 }
+	@GetMapping(value="/checkout")
+	public String pay() {
+		return "checkout";
+	}
+	
+	@PostMapping(value="/checkout")
+	
+	
+		public String returnthis() {
+		
+			cart.clear(); 
+	
+		return "redirect:/discount";
+	
+}}
