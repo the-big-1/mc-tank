94692fd6da3f8cbedcd78ddb984cb54c00720ef4
chsc949a <christoph.schroeter1@mailbox.tu-dresden.de>
2019-11-29 23:05:39 +0100

    made inventory initializer work

--- src/main/java/company18/mctank/config/InventoryDataInitializer.java
+++ src/main/java/company18/mctank/config/InventoryDataInitializer.java
@@ -1,23 +1,45 @@
 package company18.mctank.config;
 
+import java.util.Iterator;
+
+import org.salespointframework.catalog.Product;
 import org.salespointframework.core.DataInitializer;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import company18.mctank.repository.Items;
 
 @Component
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class InventoryDataInitializer implements DataInitializer {
 
 	private final UniqueInventory<UniqueInventoryItem> inv;
 	
-	public InventoryDataInitializer(UniqueInventory<UniqueInventoryItem> inv) {
+	private final Items items;
+	
+	public InventoryDataInitializer(UniqueInventory<UniqueInventoryItem> inv, Items items) {
+		Assert.notNull(inv, "Inventory must not be null");
+		Assert.notNull(items, "Items must not be null");
+		
 		this.inv = inv;
+		this.items = items;
 	}
 	
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
-		
+		Iterator<Product> iterator = this.items.findAll().iterator();
+		while (iterator.hasNext()) {
+			Product product = iterator.next();
+			// save new uniqueinventoryitem if product  not in inventory
+			if (inv.findByProduct(product) != null)
+				inv.save(new UniqueInventoryItem(product, product.createQuantity(100)));
+			// else update quantity to 100
+			else inv.findByProduct(product).map((item) -> item.increaseQuantity(product.createQuantity(100-item.getQuantity().getAmount().doubleValue())));
+		}
 	}
 
 }
--- src/main/java/company18/mctank/config/ItemsInitalizer.java
+++ src/main/java/company18/mctank/config/ItemsInitalizer.java
@@ -3,6 +3,9 @@ package company18.mctank.config;
 import org.javamoney.moneta.Money;
 import org.salespointframework.catalog.Product;
 import org.salespointframework.core.DataInitializer;
+import org.salespointframework.quantity.Metric;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
@@ -14,6 +17,7 @@ import company18.mctank.repository.Items;
 
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 class ItemsInitializer implements DataInitializer {
 	
 	private final Items items;
@@ -31,7 +35,7 @@ class ItemsInitializer implements DataInitializer {
 	public void initialize() {
 		var prod1 = new Product("Cola 0,5L", Money.of(3.00, "EUR"));
 		var prod2 = new Product("Cola 0,2L", Money.of(1.50, "EUR"));
-		var prod3 = new Product("Super Benzin", Money.of(100, "EUR"));
+		var prod3 = new Product("Super Benzin", Money.of(100, "EUR"), Metric.LITER);
 		var prod4 = new Product("McBig", Money.of(5.00, "EUR"));
 		var prod5 = new Product("Basiswäsche", Money.of(4.50, "EUR"));
 		
