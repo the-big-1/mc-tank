b6d9ea35cddd727bcc410147b75d0fd7df65377a
davlei <david.leistner@tu-dresden.de>
2019-12-05 20:08:05 +0100

    worked in itemRefill and refilltest

--- src/main/java/company18/mctank/service/RefillInventoryService.java
+++ src/main/java/company18/mctank/service/RefillInventoryService.java
@@ -6,7 +6,13 @@ import org.salespointframework.catalog.Product;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class RefillInventoryService {
 
 	@Autowired
@@ -15,24 +21,16 @@ public class RefillInventoryService {
 	@Autowired
 	private UniqueInventory<UniqueInventoryItem> inventory;
 
-	public void orderFuel(double amountToOrderBenzin, double amountToOrderDiesel /*double amountToOrderE10*/){ //new Params for more fuel types
-		//increase different fuels with the amount
-		//not more than 50.000l
-		//addToInventory() 3times for 3 different fuels
-		refillInventory("Benzin", amountToOrderBenzin);
-		refillInventory("Diesel", amountToOrderDiesel);
-	}
 
-	public void refillInventory(String prodName, double amount){
+	public void refillInventoryItem(String prodName, double amount){
 		Product product = ((Product) items.findByName(prodName));
 		inventory.findByProduct(product).map((item) -> item.increaseQuantity(product.createQuantity(amount + item.getQuantity().getAmount().doubleValue())));
 	}
 
-	/*public *DATATYPE* FuelWarning(){
-	* if (benzinamount < 10.000l) return warning
-	* else nothing
-	*
-	 */
-
-	//EventListener ??
+	public void refillInventoryItems(HashMap<String, Double> products){
+		for(Map.Entry<String, Double> entry : products.entrySet()){
+			Product product = ((Product) items.findByName(entry.getKey()));
+			inventory.findByProduct(product).map((item) -> item.increaseQuantity(product.createQuantity(entry.getValue() + item.getQuantity().getAmount().doubleValue())));
+		}
+	}
 }
--- src/test/java/company18/mctank/service/RefillInventoryServiceUnitTest.java
+++ src/test/java/company18/mctank/service/RefillInventoryServiceUnitTest.java
@@ -1,12 +1,17 @@
 package company18.mctank.service;
 
+import static org.assertj.core.api.Assertions.*;
 
 import company18.mctank.repository.ItemsRepository;
 import org.junit.jupiter.api.Test;
+import org.salespointframework.catalog.Product;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.money.Monetary;
+import javax.money.MonetaryAmount;
 
 @SpringBootTest
 public class RefillInventoryServiceUnitTest {
@@ -17,6 +22,31 @@ public class RefillInventoryServiceUnitTest {
 	@Autowired
 	private UniqueInventory<UniqueInventoryItem> inventory;
 
+	@Autowired
+	private RefillInventoryService service;
+
 	@Test
-	public void refillInventoryTest(){}
+	public void refillInventoryTest(){
+		MonetaryAmount price = Monetary.getDefaultAmountFactory()
+				.setCurrency("EUR")
+				.setNumber(1.33)
+				.create();
+
+		double testAmount = 10000;
+
+		var product1 = new Product("Benzin", price);
+		var product2 = new Product("Diesel", price);
+
+		items.save(product1);
+		items.save(product2);
+
+		inventory.save(new UniqueInventoryItem(product1, product1.createQuantity(15000)));
+		inventory.save(new UniqueInventoryItem(product2, product2.createQuantity(10000)));
+
+		service.refillInventoryItem(product1.getName(), testAmount);
+
+		assertThat(inventory.findByProduct(product1).get().getQuantity() == product1.createQuantity(25000));
+		assertThat(inventory.findByProduct(product2).get().getQuantity() == product1.createQuantity(20000));
+
+	}
 }
