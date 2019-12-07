2f7750b6cd129f735bc6db481025d640d1286c04
davlei <david.leistner@tu-dresden.de>
2019-12-07 10:34:40 +0100

    worked on fuelRefill and test, test does not work properly

--- src/main/java/company18/mctank/service/RefillInventoryService.java
+++ src/main/java/company18/mctank/service/RefillInventoryService.java
@@ -1,5 +1,6 @@
 package company18.mctank.service;
 
+import company18.mctank.exception.FuelStorageFullException;
 import company18.mctank.repository.ItemsRepository;
 
 import org.salespointframework.catalog.Product;
@@ -22,9 +23,41 @@ public class RefillInventoryService {
 
 
 	public void refillInventoryItem(String prodName, double amount){
-		var productObjekt = items.findByName(prodName).stream().findFirst();
+		var productObjekt = items.findByName(prodName)
+				 				 .stream()
+								 .findFirst();
+
 		Product product = productObjekt.get();			//can throw exception
-		inventory.findByProduct(product).map((item) -> item.increaseQuantity(product.createQuantity(amount + item.getQuantity().getAmount().doubleValue())));
+
+		inventory.findByProduct(product).map((item) -> item.increaseQuantity(product.createQuantity(amount + item.getQuantity()
+																													     .getAmount()
+																														 .doubleValue())));
+	}
+
+	public void refillFuel(String prodName, double amount) throws FuelStorageFullException{
+		var productObj = items.findByName(prodName)
+							  .stream()
+							  .findFirst();
+
+		Product product = productObj.get();			//can throw exception
+
+		var inventoryItem = inventory.findByProduct(product)
+									 .stream()
+									 .findFirst();
+
+		UniqueInventoryItem item = inventoryItem.get(); // can throw exception
+
+		double currentamount = item.getQuantity()
+								   .getAmount()
+								   .doubleValue();
+
+		if (amount + currentamount > 50000){
+			throw new FuelStorageFullException();
+		}
+
+		item.increaseQuantity(product.createQuantity(amount + item.getQuantity()
+				                                                          .getAmount()
+																		  .doubleValue()));
 	}
 
 	public void refillInventoryItems(HashMap<String, Double> products){
--- src/test/java/company18/mctank/service/RefillInventoryServiceUnitTest.java
+++ src/test/java/company18/mctank/service/RefillInventoryServiceUnitTest.java
@@ -28,25 +28,33 @@ public class RefillInventoryServiceUnitTest {
 	@Test
 	public void refillInventoryTest(){
 		MonetaryAmount price = Monetary.getDefaultAmountFactory()
-				.setCurrency("EUR")
-				.setNumber(1.33)
-				.create();
+									   .setCurrency("EUR")
+									   .setNumber(1.33)
+									   .create();
 
 		double testAmount = 10000;
+		double failAmount = 50000;
 
 		var product1 = new Product("Benzin", price);
 		var product2 = new Product("Diesel", price);
 
+		var exceptionProd = new Product("E10", price);
+
 		items.save(product1);
 		items.save(product2);
+		items.save(exceptionProd);
 
 		inventory.save(new UniqueInventoryItem(product1, product1.createQuantity(15000)));
 		inventory.save(new UniqueInventoryItem(product2, product2.createQuantity(10000)));
+		inventory.save(new UniqueInventoryItem(exceptionProd, exceptionProd.createQuantity(10000)));
+
 
 		service.refillInventoryItem(product1.getName(), testAmount);
 
-		assertThat(inventory.findByProduct(product1).get().getQuantity() == product1.createQuantity(25000));
-		assertThat(inventory.findByProduct(product2).get().getQuantity() == product1.createQuantity(20000));
+		assertThat(inventory.findByProduct(product1).get().getQuantity().getAmount().doubleValue() == 25000);
+		assertThat(inventory.findByProduct(product2).get().getQuantity().getAmount().doubleValue() == 20000);  //is true but 10000 != 20000
+
+		//does not work, dont know why
 
 	}
 }
