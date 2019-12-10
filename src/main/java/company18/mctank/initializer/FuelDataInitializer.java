49c1f57e31b3f14fab01b640f0f1f8c7e3bbfb9b
davlei <david.leistner@tu-dresden.de>
2019-12-10 18:10:10 +0100

    problem with initializer

--- src/main/java/company18/mctank/initializer/FuelDataInitializer.java
+++ src/main/java/company18/mctank/initializer/FuelDataInitializer.java
@@ -14,7 +14,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
-@Order(10)
 public class FuelDataInitializer implements DataInitializer {
 
 	@Autowired
@@ -26,7 +25,7 @@ public class FuelDataInitializer implements DataInitializer {
 	@Override
 	public void initialize(){
 
-		var fuel_1 = new Product("Super Benzin", Money.of(1.33, "EUR"), Metric.LITER);
+		/*var fuel_1 = new Product("Super Benzin", Money.of(1.33, "EUR"), Metric.LITER);
 		var fuel_2 = new Product("Diesel", Money.of(1.29, "EUR"), Metric.LITER);
 
 		fuel_1.addCategory("McZapf");
@@ -38,6 +37,6 @@ public class FuelDataInitializer implements DataInitializer {
 		inventory.save(new UniqueInventoryItem(fuel_1, fuel_1.createQuantity(1000)));
 		inventory.save(new UniqueInventoryItem(fuel_2, fuel_2.createQuantity(1000)));
 
-
+		 */
 	}
 }
