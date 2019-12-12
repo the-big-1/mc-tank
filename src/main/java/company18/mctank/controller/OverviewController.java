a31551a4debe8cc5499d3ae6583c51320cb2c9b6
davlei <david.leistner@tu-dresden.de>
2019-12-12 17:08:18 +0100

    a bit of progress on fuel warning

--- src/main/java/company18/mctank/controller/OverviewController.java
+++ src/main/java/company18/mctank/controller/OverviewController.java
@@ -3,8 +3,7 @@ package company18.mctank.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 @PreAuthorize("hasRole('ADMIN')")
